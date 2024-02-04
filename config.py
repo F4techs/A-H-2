b07456b9749a89aa95016eb5191899b3611dbca0
F X S I L <86817323+F4techs@users.noreply.github.com>
2024-02-04 22:39:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,7 @@ from os import getenv
 load_dotenv()
 
 BOT_TOKEN = getenv('TOKEN')
-PLACEHOLDER = getenv('PLACEHOLDER')
 CLEAR_CONTEXT_ANSWER = getenv('CLEAR_CONTEXT_ANSWER')
 CONTEXT_SIZE = int(getenv('CONTEXT_SIZE'))
-ALLOWED_IDS = list(map(lambda x: int(x.strip()), getenv('ALLOWED_IDS').split(',')))
 START_PLACEHOLDER = getenv('START_PLACEHOLDER')
 
